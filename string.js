80dcdf997a6a4f1e33fe1e89d9653d55dd026dac
Student <daisychepkiruih@gmail.com>
2024-03-14 09:02:11 +0300

    add file

---
+++ string.js
@@ -0,0 +1,40 @@
+//Concatenate the strings "Hello" and "World" in JavaScript to create the message "Hello World."
+let h="Hello";
+let i="World";
+let k=h+ " "+i;
+console.log(k)
+
+//Use the + operator to combine the variables firstName and lastName into a full name.
+const firstName ="Daisy";
+const lastName = "Chepkirui";
+const  fullName= firstName+" "+lastName;
+console.log(fullName);
+
+//Create a template literal in JavaScript to display the current year dynamically. For example, "The current year is 2024."
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+console.log(currentYear);
+
+
+
+//Access the third element in an array and print its value.
+let  array =[1,2,3,4];
+let thirdElement=array[2];
+console.log(thirdElement);
+
+//Use string interpolation to insert the value of a variable age into the string "I am years old."
+let age= 22;
+let text =`I am ${age}years old`;
+console.log(text);
+//Calculate the sum of two numbers, num1 and num2, and print the result
+let a=20;
+let b=24;
+let c=a+b;
+console.log(c);
+
+//Multiply the variable quantity by the variable price to calculate the total cost.
+let quantity1=50;
+let quantity2=60;
+let totalCost= quantity1*quantity2;
+console.log(totalCost)
+
